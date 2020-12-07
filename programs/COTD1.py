08f8412e2d59e604cb86fc841ffedc1843ed98f4
allenokech <allen.okech@gmail.com>
2020-12-07 19:16:34 +0000

    Modified Challenge of the day #1

--- programs/COTD1.py
+++ programs/COTD1.py
@@ -1,13 +1,14 @@
+#def sorting(myString):
+ #   words = myString.split()
+   # noDuplicates = sorted(set(words))
+    #return " ".join(noDuplicates)
+
+#print(sorting("hello world and practice makes perfect and hello world again"))
+
 def sorting(myString):
     words = myString.split()
-    words.sort()
     noDuplicates = sorted(set(words))
     return " ".join(noDuplicates)
 
-print(sorting("hello world and practice makes perfect and hello world again"))
-
-#def sorting(myString):
- #   myString = input("Enter phrase: ")
-  #  words = myString.split()
-   # words.sort()
-    #return " ".join(words)
+myString = input("Enter: ")
+print(sorting(myString))
